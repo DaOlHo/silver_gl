9dc511492f3a92ade48369ab594a7a2542702172
Maya Hodul <dhodul@gmail.com>
2023-01-28 20:40:21 -0800

    UBO can now bind to support multiple

--- src/uniform_buffer.rs
+++ src/uniform_buffer.rs
@@ -4,21 +4,23 @@ use super::GlError;
 // UBO can have multiple types of data, so it doesn't have a type
 pub struct UniformBuffer {
     id: u32,
-    name: String
+    name: String,
+    buffer_size: isize
 }
 
 impl UniformBuffer {
-    pub fn new(shader_programs: Vec<&ShaderProgram>, name: &str, buffer_size: u32) -> Result<UniformBuffer, GlError> {
+    pub fn new(shader_programs: Vec<&ShaderProgram>, name: &str, buffer_size: isize) -> Result<UniformBuffer, GlError> {
         let mut uniform_buffer = UniformBuffer {
             id: 0,
-            name: String::from(name)
+            name: String::from(name),
+            buffer_size
         };
 
         for shader_program in shader_programs.iter() {
             uniform_buffer.register_shader_program(shader_program)?;
         }
 
-        uniform_buffer.create_ubo(buffer_size);
+        uniform_buffer.create_ubo();
 
         Ok(uniform_buffer)
     }
@@ -27,11 +29,17 @@ impl UniformBuffer {
         shader_program.bind_to_ubo(self.name.as_str())
     }
 
-    pub fn create_ubo(&mut self, buffer_size: u32) {
+    pub fn create_ubo(&mut self) {
         unsafe {
             gl::CreateBuffers(1, &mut self.id);
-            gl::NamedBufferData(self.id, buffer_size as isize, std::ptr::null(), gl::DYNAMIC_DRAW);
-            gl::BindBufferRange(gl::UNIFORM_BUFFER, 0, self.id, 0, buffer_size as isize);
+            gl::NamedBufferData(self.id, self.buffer_size, std::ptr::null(), gl::DYNAMIC_DRAW);
+            gl::BindBufferRange(gl::UNIFORM_BUFFER, 0, self.id, 0, self.buffer_size);
+        }
+    }
+
+    pub fn bind_ubo(&self) {
+        unsafe {
+            gl::BindBufferRange(gl::UNIFORM_BUFFER, 0, self.id, 0, self.buffer_size);
         }
     }
 
